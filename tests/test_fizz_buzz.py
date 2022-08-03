58404ba484a72fc078627a77d64857aa20212319
Sergey Martynov <acemore@mail.ru>
2022-08-03 03:03:07 +0300

    Add tests for fizz_buzz package

---
+++ tests/test_fizz_buzz.py
@@ -0,0 +1,15 @@
+from fizz_buzz.fizz_buzz import (
+    BUZZ, BUZZ_NUMBER, FIZZ, FIZZ_BUZZ, FIZZ_NUMBER, fizz_buzz
+)
+
+FIZZ_BUZZ_NUMBER = 15
+JUST_NUMBER = 1
+NOT_A_DIGIT = 'Not a digit'
+
+
+def test_fizz_buzz():
+    assert fizz_buzz(JUST_NUMBER) == JUST_NUMBER
+    assert fizz_buzz(FIZZ_NUMBER) == FIZZ
+    assert fizz_buzz(BUZZ_NUMBER) == BUZZ
+    assert fizz_buzz(FIZZ_BUZZ_NUMBER) == FIZZ_BUZZ
+    assert fizz_buzz(NOT_A_DIGIT) is None
