cf6b76e7e5c2940dff7267cff2504cb150932e8b
Acemore <91070379+Acemore@users.noreply.github.com>
2022-08-05 09:31:16 +0300

    Remove constants from fizz_buzz package

--- fizz_buzz/fizz_buzz.py
+++ fizz_buzz/fizz_buzz.py
@@ -1,24 +1,17 @@
-BUZZ = 'Buzz!'
-BUZZ_NUMBER = 5
-FIZZ = 'Fizz!'
-FIZZ_BUZZ = 'FizzBuzz!'
-FIZZ_NUMBER = 3
-
-
 def fizz_buzz(number):
     if not isinstance(number, int):
         return
 
-    divisible_by_3 = not number % FIZZ_NUMBER
-    divisible_by_5 = not number % BUZZ_NUMBER
+    divisible_by_3 = not number % 3
+    divisible_by_5 = not number % 5
 
     result = number
 
     if divisible_by_3 and divisible_by_5:
-        result = FIZZ_BUZZ
+        result = 'FizzBuzz!'
     elif divisible_by_5:
-        result = BUZZ
+        result = 'Buzz!'
     elif divisible_by_3:
-        result = FIZZ
+        result = 'Fizz!'
 
     return result
